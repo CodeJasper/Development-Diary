5ed662699b00a70af55a25263e5a3078fa257d9a
jasper <jasper@jasper.jasper>
2025-07-16 18:02:22 -0500

    feat: add initial credentials provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,27 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          type: "email",
+          label: "Email",
+          placeholder: "johndoe@gmail.com",
+        },
+        password: {
+          type: "password",
+          label: "Password",
+          placeholder: "*****",
+        },
+      },
+      authorize: async (credentials) => {
+        let user = null
+        return user
+      },
+    }),
+    Google],
 })
