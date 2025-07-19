f1d698c7d0e58cca46bb1668bd996f6a1a2556af
jasper <jasper@jasper.jasper>
2025-07-19 04:22:37 -0500

    feat: handle validations in register form

--- src/app/register/actions.ts
+++ src/app/register/actions.ts
@@ -5,31 +5,75 @@ import { prisma } from "@/lib/prisma"
 import { redirect } from "next/navigation"
 import { generateVerificationToken } from "@/lib/tokens"
 import { sendVerificationEmail } from "@/lib/mail"
+import * as yup from "yup"
+import { RegistrationFormProps } from "./page"
 
-export async function registerUser(formData: FormData) {
-  const email = formData.get("email") as string
-  const password = formData.get("password") as string
+const schema = yup.object({
+  userName: yup.string().required("El nombre de usuario es obligatorio"),
+  email: yup.string().email("Correo inválido").required("El correo es obligatorio"),
+  password: yup.string().min(6, "Mínimo 6 caracteres").required("La contraseña es obligatoria"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Las contraseñas no coinciden")
+    .required("Confirma tu contraseña"),
+})
 
-  if (!email || !password) return
+export type FormState<T> = {
+  fieldErrors?: Partial<Record<keyof T, string>>;
+  generalErrors?: string[];
+  data?: Partial<T>;
+  success?: boolean
+}
 
-  const existing = await prisma.user.findUnique({ where: { email } })
-  if (existing) return
+export async function registerUser(_: FormState<RegistrationFormProps>, data: FormData) {
+  const password = data.get('password') as string;
+  const email = data.get('email') as string;
+  const userName = data.get('userName') as string;
+  const confirmPassword = data.get('confirmPassword') as string;
+
+  try {
+    await schema.validate({ password, email, userName, confirmPassword}, { abortEarly: false });
+  } catch (err) {
+    const validationErrors = {};
+    err.inner.forEach((error) => {
+      if (error.path && !validationErrors[error.path]) {
+        validationErrors[error.path] = error.message;
+      }
+    });
+    return { fieldErrors: validationErrors, data: { email, userName } };
+  }
+
+  if (!email || !password || !userName) return
+
+  const existing = await prisma.user.findFirst({
+    where: {
+      OR: [
+        { email },
+        { userName },
+      ],
+    },
+  })
+
+  if (existing?.email === email) {
+    return { generalErrors: ["Este correo ya está registrado."] }
+  }
+
+  if (existing?.userName === userName) {
+    return { generalErrors: ["Este nombre de usuario ya está registrado."] }
+  }
 
   const hashed = await hash(password, 10)
-
   const token = generateVerificationToken();
 
   await prisma.user.create({
     data: {
+      userName,
       email,
       password: hashed,
-      name: email.split("@")[0],
-      lastName: "Doe",
       verificationToken: token,
     },
   })
 
   sendVerificationEmail(email, token)
-
-  redirect("/login")
+  redirect('/login');
 }
