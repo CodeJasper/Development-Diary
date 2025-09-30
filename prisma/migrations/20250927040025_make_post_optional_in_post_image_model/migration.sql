d3ff3e4d2af7fd48add6df8935cbd406fd627daf
CodeJasper <jssv0116@hotmail.com>
2025-09-29 22:43:13 -0500

    feat: add migrations of PostImage model

---
+++ prisma/migrations/20250927040025_make_post_optional_in_post_image_model/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE "PostImage" DROP CONSTRAINT "PostImage_postId_fkey";
+
+-- AlterTable
+ALTER TABLE "PostImage" ALTER COLUMN "postId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "PostImage" ADD CONSTRAINT "PostImage_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ prisma/migrations/20250927040653_remove_unnecessary_property_in_post_image_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageId` on the `PostImage` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PostImage" DROP COLUMN "imageId";
